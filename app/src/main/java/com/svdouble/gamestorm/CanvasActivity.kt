e73172ea0d4e1b8f5364708c7f2b4351947a9d00
IntelliJ User <se2620@yandex.ru>
2018-06-06 15:03:19 +0300

    Implement shapes for tic-tac-toe

--- app/src/main/java/com/svdouble/gamestorm/CanvasActivity.kt
+++ app/src/main/java/com/svdouble/gamestorm/CanvasActivity.kt
@@ -1,5 +1,6 @@
 package com.svdouble.gamestorm
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
@@ -9,29 +10,55 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.MotionEvent
 import android.graphics.Paint.Style.STROKE
+import android.graphics.Path
 import android.widget.TextView
+import android.text.method.Touch.onTouchEvent
+import android.util.Log
+
+const val TAG = "GameStorm"
 
 
 class Draw2D(context: Context) : View(context) {
     private val mPaint = Paint()
+    private val shadowPaint = Paint()
+    private var xPath: Float = 0.0f
+    private var yPath: Float = 0.0f
+    private var urg:Int = 0
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            this.xPath = event.x
+            this.yPath = event.y
+            Log.d(TAG, "Touch detected!")
+            urg+=1
+            this.invalidate()
+        }
+        return super.onTouchEvent(event)
+    }
 
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+
         //крестик
         fun Cross(arg1: Float, arg2: Float) {
+            val arg11:Float = arg1 - (arg1 % 60) + 30f
+            val arg22:Float = arg2 + 30f - (arg2 % 60)
             mPaint.color = Color.BLACK
-            mPaint.strokeWidth = 5F
-            canvas.drawLine(arg1-15F, arg2-15F, arg1+15F, arg2+15F, mPaint)
-            canvas.drawLine(arg1+15F, arg2-15F, arg1-15F, arg2+15F, mPaint)
+            mPaint.strokeWidth = 7F
+            canvas.drawLine(arg11 - 25F, arg22 - 25F, arg11 + 25F, arg22 + 25F, mPaint)
+            canvas.drawLine(arg11 + 25F, arg22 - 25F, arg11 - 25F, arg22 + 25F, mPaint)
         }
+
         //нолик
         fun Circle(arg1: Float, arg2: Float) {
-            mPaint.setAntiAlias(true)
+            val arg11:Float = arg1 - (arg1 % 60) + 30f
+            val arg22:Float = arg2 + 30f - (arg2 % 60)
+            mPaint.isAntiAlias = true
             mPaint.color = Color.GREEN // установим зеленый цвет
             mPaint.style = STROKE
-            mPaint.setStrokeWidth(5F)
-            canvas.drawCircle(arg1, arg2, 14f, mPaint)
+            mPaint.strokeWidth = 5F
+            canvas.drawCircle(arg11, arg22, 27f, mPaint)
         }
 
         mPaint.color = Color.YELLOW
@@ -43,48 +70,38 @@ class Draw2D(context: Context) : View(context) {
         var i = 0F
 
         mPaint.color = Color.RED
-        mPaint.strokeWidth = 5F
-        while(i <= h*3/4) {
+        mPaint.strokeWidth = 7F
+        while (i <= h * 3 / 4) {
             canvas.drawLine(i, 0F, i, w, mPaint)
-            i+=40F
+            i += 60F
         }
         i = 0F
-        while(i+80F <= h*3/4) {
+        while (i + 80f<= h * 3 / 4) {
             canvas.drawLine(0F, i, h, i, mPaint)
-            i+=40F
+            i += 60F
         }
-
-
-        val shadowPaint = Paint()
         shadowPaint.isAntiAlias = true
         shadowPaint.color = Color.BLUE
         shadowPaint.textSize = 45.0f
         shadowPaint.strokeWidth = 3.0f
         shadowPaint.style = Paint.Style.FILL
         shadowPaint.setShadowLayer(5.0f, 10.0f, 10.0f, Color.GRAY)
-        canvas.drawText("моя программа, ребят", 20f, 510f, shadowPaint)
-        canvas.drawText("X:$x   Y:$y", 20f, 540f, shadowPaint)
-
+        shadowPaint.textSize = 50.0f
+        shadowPaint.strokeWidth = 4.0f
+        canvas.drawText("$xPath    $yPath", 20f, 540f, shadowPaint)
+        if(urg % 2 == 0)
+          Circle(xPath,yPath)
+        else
+          Cross(xPath,yPath)
     }
 }
 
-
-
 class CanvasActivity : AppCompatActivity() {
-    internal var x: Float = 0.toFloat()
-    internal var y: Float = 0.toFloat()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val draw2D = Draw2D(this)
         setContentView(draw2D)
        }
-
-    fun onTouch(event: MotionEvent): Boolean {
-        x = event.x
-        y = event.y
-        return true
-    }
-
 }
 
