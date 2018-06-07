1b860f19c0209afe0bb2b18afe09d6fa45ea7911
IntelliJ User <se2620@yandex.ru>
2018-06-07 13:31:56 +0300

    Implement graphics library

--- app/src/main/java/com/svdouble/gamestorm/CanvasActivity.kt
+++ app/src/main/java/com/svdouble/gamestorm/CanvasActivity.kt
@@ -1,137 +1,63 @@
 package com.svdouble.gamestorm
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Color.BLUE
+import android.graphics.Color.*
 import android.graphics.Paint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.MotionEvent
 import android.graphics.Paint.Style.STROKE
-import android.graphics.Path
-import android.widget.TextView
-import android.text.method.Touch.onTouchEvent
 import android.util.Log
 
 const val TAG = "GameStorm"
-const val bottomMargin : Float = 3f / 4f
-const val squareSide : Int = 60
+const val bottomMargin: Float = 3f / 4
 
+/* Handle calls */
+sealed class BaseCall
 
-class Cell( num1:Int, y1:Float)
-{
-    var x:Float = (num1 * squareSide).toFloat()
-    var y:Float = y1
-    var bottom: Float = y1 + squareSide
-    var right: Float = x + squareSide
-    var type: Int = 0
-    var num : Int = num1
-    var bgcolor  = Color.TRANSPARENT
+class CallDrawGrid(val rows: Int, val columns: Int, val view: View) : BaseCall()
+class CallDrawChip(val point: Cell2D, val chipId: Int) : BaseCall()
+class CallDrawGridCells(val points: Array<Cell2D>, val color: Int) : BaseCall()
 
-    fun Draw( mPaint : Paint, canvas : Canvas )
-    {
-        if( type == 1 )
-            Cross(mPaint,canvas)
-        else if( type == 2 )
-            Circle(mPaint,canvas)
-    }
-
-    //крестик
-    fun Cross( mPaint : Paint, canvas : Canvas ) {
-        val arg11:Float = x - (x % side()) + side()/2
-        val arg22:Float = y + side()/2 - (y % side())
-        mPaint.color = Color.BLACK
-        mPaint.strokeWidth = 7F
-        canvas.drawLine(arg11 - (side()/2 - 5f), arg22 - (side()/2 - 5f), arg11 + (side()/2 - 5f), arg22 + (side()/2 - 5f), mPaint)
-        canvas.drawLine(arg11 + (side()/2 - 5f), arg22 - (side()/2 - 5f), arg11 - (side()/2 - 5f), arg22 + (side()/2 - 5f), mPaint)
-    }
-
-    fun side() : Float
-    {
-        return squareSide.toFloat()
-    }
-
-    //нолик
-    fun Circle( mPaint : Paint, canvas : Canvas ) {
-        val arg11:Float = x - (x % side()) + side()/2
-        val arg22:Float = y + side()/2 - (y % side())
-        mPaint.isAntiAlias = true
-        mPaint.color = Color.GREEN // установим зеленый цвет
-        mPaint.style = STROKE
-        mPaint.strokeWidth = 5F
-        canvas.drawCircle(arg11, arg22, side()/2 - 4f, mPaint)
-    }
-
-    fun draw(mPaint : Paint, canvas : Canvas)
-    {
-        mPaint.color = bgcolor
-        canvas.drawRect( x,y,right,bottom,mPaint )
-        mPaint.color = Color.RED
-        mPaint.strokeWidth = 7F
-        canvas.drawLine( right, y, right, bottom, mPaint )
-        canvas.drawLine( x, bottom, right, bottom, mPaint )
-        if( type == 2 )
-            Cross(mPaint,canvas)
-        else if( type == 1 )
-            Circle(mPaint,canvas)
-    }
-
-    fun onTouch( x1 : Float, y1 : Float, type1 : Int ) : Boolean
-    {
-        if( x1 < x || y1 < y || y1 > bottom || x1 > right || type != 0)
-            return false
-        type = type1
-        return true;
-    }
-
-}
-
-class Row( num1 : Int, width : Int )
-{
-    var num : Int = num1
-    var arr = emptyArray<Cell>()
+class DrawStorage(private val draw: Draw2D) {
 
     init {
-        this.arr = Array<Cell>( width/ squareSide ,{i->Cell(i,(num1* squareSide).toFloat())})
-
+        Log.d(TAG, "Storage created!")
     }
 
-
-    fun draw( mPaint : Paint, canvas : Canvas )
-    {
-//        canvas.drawLine( 0f, arr[0].y + arr[0]., w, (ii * squareSide).toFloat(), mPaint )
-        for( el in arr )
-            el.draw(mPaint,canvas)
+    val drawCalls: ArrayList<BaseCall> = arrayListOf()
+    fun drawFromStorage(canvas: Canvas) {
+        Log.d(TAG, "Start drawing from storage!")
+        for (call in drawCalls)
+            when (call) {
+                is CallDrawGridCells -> draw.fillGridCells(call, canvas)
+                is CallDrawChip -> draw.drawChip(call, canvas)
+                is CallDrawGrid -> draw.drawGrid(call, canvas)
+            }
     }
-
-    fun bottom() : Float
-    {
-        return arr[ arr.size - 1 ].bottom
-    }
-
-    fun onTouch( x : Float, y : Float, type : Int ) : Cell?
-    {
-        for( el in arr )
-            if( el.onTouch( x, y, type ) )
-                return el
-        return null
-    }
-
 }
 
+
 class Draw2D(context: Context) : View(context) {
+
+    val storage = DrawStorage(this)
+    private var rown = 0
     private val mPaint = Paint()
     private val shadowPaint = Paint()
     private var xPath: Float = 0.0f
     private var yPath: Float = 0.0f
-    private var urg:Int = 0
-    private var k: Int = 0
-    private var rows : Array<Row> = emptyArray()
+    private var w: Int = width
+    private var h: Int = height
+
+    private var urg: Int = 0
 
 
+    private var squareSide: Int = 0
+
+    private val numHorizLines: Float = rown.toFloat()
+    private val lastLineY = numHorizLines * squareSide
 
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
@@ -139,8 +65,8 @@ class Draw2D(context: Context) : View(context) {
             this.xPath = event.x
             this.yPath = event.y
             Log.d(TAG, "Touch detected!")
-            urg = ( urg + 1 ) % 2
-            for ( row in rows ) {
+            urg = (urg + 1) % 2
+            /*for ( row in rows ) {
 
                 val cell = row.onTouch(event.x, event.y, urg + 1)
                 if ( cell != null ) {
@@ -157,7 +83,7 @@ class Draw2D(context: Context) : View(context) {
 
                     break
                 }
-            }
+            }*/
             this.invalidate()
         }
         return super.onTouchEvent(event)
@@ -165,71 +91,103 @@ class Draw2D(context: Context) : View(context) {
 
 
     override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
 
+        super.onDraw(canvas)
+        w = width
+        h = height
+        Log.d(TAG, "width $w")
         //фон
-        mPaint.color = Color.YELLOW
+        mPaint.color = YELLOW
         mPaint.style = Paint.Style.FILL
         canvas.drawPaint(mPaint)
 
-        if( rows.isEmpty() )
-        {
-            rows  = Array<Row>( ( height.toFloat() * bottomMargin / squareSide).toInt() - 1, {i->Row(i,width) } )
-        }
+        storage.drawFromStorage(canvas)
 
-        for( el in rows )
-            el.draw(mPaint,canvas)
-// линии
-//        val w: Float = width.toFloat()
-//        val h: Float = height.toFloat()
-//        mPaint.color = Color.RED
-//        val strokewidth:Float = 7F
-//        mPaint.strokeWidth = strokewidth
-//        val numHorizLines : Float = (h * bottomMargin / squareSide).toInt().toFloat() - 1
-//        val lastLineY = numHorizLines * squareSide
-//        val numVertLines : Float = w / squareSide
-//        //горизонтальные
-//        for( ii in 0..numHorizLines.toInt() )
-//        {
-//            canvas.drawLine( 0f, (ii * squareSide).toFloat(), w, (ii * squareSide).toFloat(), mPaint )
-//        }
-//        //вертикальные
-//        for( ii in 0..numVertLines.toInt() )
-//        {
-//            canvas.drawLine( (ii*squareSide).toFloat(), 0f, (ii*squareSide).toFloat(), lastLineY, mPaint )
-//        }
-
-        //текст
+        /*
         shadowPaint.isAntiAlias = true
         shadowPaint.color = BLUE
         shadowPaint.textSize = 45.0f
         shadowPaint.strokeWidth = 3.0f
         shadowPaint.style = Paint.Style.FILL
-        shadowPaint.setShadowLayer(5.0f, 10.0f, 10.0f, Color.GRAY)
+        shadowPaint.setShadowLayer(5.0f, 10.0f, 10.0f, GRAY)
         shadowPaint.textSize = 50.0f
         shadowPaint.strokeWidth = 4.0f
-        canvas.drawText("$xPath    $yPath", 20f, rows[ rows.size - 1 ].bottom() + 50f, shadowPaint)
+        canvas.drawText("$xPath    $yPath", 20f, /*rows[ rows.size - 1 ].bottom()*/lastLineY + 50f, shadowPaint) */
+    }
 
+    fun drawGrid(call: CallDrawGrid, canvas: Canvas) {
+        var squareSide: Int = w / call.columns
+        val lastLineY = 1f * call.rows * squareSide
 
+        if (w > (h * bottomMargin))
+            squareSide = (h * bottomMargin / rown).toInt()
 
-//        if(yPath < lastLineY) {
-//            if (urg % 2 == 0) {
-//                Circle(xPath, yPath, squareSide)
-//
-//            }
-//            else {
-//                Cross(xPath, yPath, squareSide)
-//            }
-//        }
+        this.squareSide= squareSide
+        rown = call.rows
+
+        mPaint.strokeWidth = 7F
+        mPaint.color = RED
+
+        for (ii in 0..call.rows) {
+            canvas.drawLine(0f, (ii * squareSide).toFloat(), w.toFloat(), (ii * squareSide).toFloat(), mPaint)
+        }
+        for (ii in 0..call.columns) {
+            canvas.drawLine((ii * squareSide).toFloat(), 0f, (ii * squareSide).toFloat(), lastLineY, mPaint)
+        }
+    }
+
+    fun drawChip(call: CallDrawChip, canvas: Canvas) {
+        when (call.chipId) {
+            0 -> { // cross
+                val xxx = (call.point.x * 1f * squareSide - 1)
+                val yyy = (call.point.y * 1f * squareSide - 1)
+                val arg11: Float = xxx - (xxx % squareSide) + squareSide / 2
+                val arg22: Float = yyy + squareSide / 2 - (yyy % squareSide)
+                mPaint.color = BLACK
+                mPaint.strokeWidth = 7F
+                canvas.drawLine(arg11 - (squareSide / 2 - 5f), arg22 - (squareSide / 2 - 5f), arg11 + (squareSide / 2 - 5f), arg22 + (squareSide / 2 - 5f), mPaint)
+                canvas.drawLine(arg11 + (squareSide / 2 - 5f), arg22 - (squareSide / 2 - 5f), arg11 - (squareSide / 2 - 5f), arg22 + (squareSide / 2 - 5f), mPaint)
+            }
+
+            1 -> { // circle
+                val xxx = 1f * call.point.x * squareSide - 1
+                val yyy = 1f * call.point.y * squareSide - 1
+                val arg11: Float = xxx - (xxx % squareSide) + squareSide / 2
+                val arg22: Float = yyy + squareSide / 2 - (yyy % squareSide)
+
+                mPaint.isAntiAlias = true
+                mPaint.color = GREEN // установим зеленый цвет
+                mPaint.style = STROKE
+                mPaint.strokeWidth = 6F
+                canvas.drawCircle(arg11, arg22, squareSide / 2 - 4f, mPaint)
+
+            }
+        }
 
     }
+
+    fun fillGridCells(call: CallDrawGridCells, canvas: Canvas) {
+        mPaint.color = call.color
+        call.points.forEach {
+            val xxx = ((it.x - 1f) * squareSide)
+            val yyy = ((it.y - 1f) * squareSide)
+            val bottom: Float = yyy + squareSide
+            val right: Float = xxx + squareSide
+            canvas.drawRect(xxx, yyy, right, bottom, mPaint)
+        }
+    }
 }
+
+data class Cell2D(val x: Int, val y: Int)
 
 class CanvasActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val draw2D = Draw2D(this)
+        draw2D.storage.drawCalls.add(CallDrawGrid(5, 5, draw2D))
+        draw2D.storage.drawCalls.add(CallDrawChip(Cell2D(1, 2), 0))
+        draw2D.storage.drawCalls.add(CallDrawGridCells(arrayOf(Cell2D(2, 2), Cell2D(2, 3)), BLACK))
         setContentView(draw2D)
     }
 }
