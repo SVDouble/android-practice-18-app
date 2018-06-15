c2129ab644fcf4f16427b384782565658398f3b5
SVDouble <svdouble@hotmail.com>
2018-06-15 04:05:14 +0300

    Fix TGame canvas

--- app/src/main/java/com/svdouble/gamestorm/CanvasActivity.kt
+++ app/src/main/java/com/svdouble/gamestorm/CanvasActivity.kt
@@ -3,6 +3,7 @@ package com.svdouble.gamestorm
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_base_canvas.*
+import org.jetbrains.anko.displayMetrics
 
 const val TAG = "GameStorm"
 
@@ -14,13 +15,17 @@ class CanvasActivity : AppCompatActivity() {
         when(intent.getIntExtra(INTENT_ID_KEY, -1)) {
             GAME_TICTACTOE_ID -> {
                 setContentView(R.layout.activity_base_canvas)
+
+                /* Attention: linear layout returns strange width / height */
+                val maxH = displayMetrics.heightPixels - bc_header.layoutParams.height - bc_footer.layoutParams.height
+                val maxW = displayMetrics.widthPixels
+                val maxSide = if (maxH > maxW) maxW else maxH
+                bc_content.layoutParams.width = maxSide
+                bc_content.layoutParams.height = maxSide
+
                 val tGame = Games.getInstance(bc_content.context).games[0] as TGame
                 bc_header_title.text = resources.getString(tGame.titleRId)
                         bc_content.addView(tGame.getDrawEngine())
-                if (bc_content.layoutParams.width > bc_content.layoutParams.height)
-                    bc_content.layoutParams.width = bc_content.layoutParams.height
-                else
-                    bc_content.layoutParams.height = bc_content.layoutParams.width
                 if (tGame.getState() == BaseGameHandler.State.INIT)
                     tGame.startGame()
             }
